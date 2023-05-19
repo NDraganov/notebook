5110aef5c9246c9801aa59100259e2f2e280dae8
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-05-20 00:34:03 +0100

    Add icon to the header

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,8 +1,11 @@
+import { CgNotes } from "react-icons/cg";
+
 import classes from "./header.module.css";
 
 export default function Header({ text }) {
   return (
     <header className={classes.header}>
+      <CgNotes />
       <h2>{text}</h2>
     </header>
   );

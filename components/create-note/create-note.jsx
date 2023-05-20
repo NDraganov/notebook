29f878c4e8b6a3d0e926c2a6c3bf6075ce925ee7
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-05-20 10:14:55 +0100

    Create state for the note and handle on change event for the inputs

--- components/create-note/create-note.jsx
+++ components/create-note/create-note.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 import classes from "./create-note.module.css";
 
 export default function CreateNote() {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setNote(value);
+  };
   return (
     <div className={classes.note}>
       <form>
-        <input type="text" name="title" placeholder="Title" />
+        <input
+          type="text"
+          name="title"
+          value={note.title}
+          onChange={handleChange}
+          placeholder="Title"
+        />
         <textarea
           name="content"
+          value={note.content}
+          onChange={handleChange}
           rows="7"
           placeholder="Take a note ..."
         ></textarea>
